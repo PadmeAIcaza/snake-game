92140ca1a4228953ac0ae869f56bb118a02e6a1b
spicypusheen <padme.garrapata@gmail.com>
2025-12-19 09:02:00 -0600

    Adding a new class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,31 +8,14 @@ screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0) # turn off the tracer
 
-# starting positions the three starting squares
-starting_positions = [(0, 0), (-20, 0), (-40, 0)] # tuples
-
-# all the new positions of the squares will be stored here
-segments = []
-
-# loops to create all 3 squares
-for position in starting_positions:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup() # it won't draw the default line
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_on = True
 while game_on:
     screen.update() # updates the screen once all squares have move forward
     time.sleep(0.1) # adds a 0.1 second delay after all squares have moved
-                            # start  stop  step
-    for seg_num in range(len(segments) - 1, 0, -1): # this for loop will make the snake move continuously without disconnecting
-        new_x = segments[seg_num - 1].xcor() # grabs the previous square's xcoord
-        new_y = segments[seg_num - 1].ycor() # grabs the previous square's ycoord
-        segments[seg_num].goto(new_x, new_y) # sets the previous square's x and y coords into next square
-    segments[0].forward(20)
-    segments[0].left(90)
+
+    snake.move()
 
 
 
---
+++ snake.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+# starting positions the three starting squares
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)] # tuples
+MOVE_DISTANCE = 20
+
+class Snake:
+    def __init__(self):
+        # all the new positions of the squares will be stored here
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        '''creates snake's body'''
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle('square')
+            new_segment.color('white')
+            new_segment.penup()  # it won't draw the default line
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        # start  stop  step
+        for seg_num in range(len(self.segments) - 1, 0,
+                             -1):  # this for loop will make the snake move continuously without disconnecting
+            new_x = self.segments[seg_num - 1].xcor()  # grabs the previous square's xcoord
+            new_y = self.segments[seg_num - 1].ycor()  # grabs the previous square's ycoord
+            self.segments[seg_num].goto(new_x, new_y)  # sets the previous square's x and y coords into next square
+        self.segments[0].forward(MOVE_DISTANCE)
+        self.segments[0].left(90)
